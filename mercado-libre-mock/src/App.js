60b25bec1fbdb1bc5c209f53aaefdc7d0551101b
mabenunez <39781996+mabenunez@users.noreply.github.com>
2019-09-02 16:19:18 -0300

    react app w folder structure and proto-navbar in App

---
+++ mercado-libre-mock/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+
+        <div className={'navbar1 flex-column'}>
+          <div className={'flex-row navbar2'}>
+            <div>
+              sadfcsd
+              <img></img>
+            </div>
+            <input className={'navbar-input'} type='text' placeholder='Buscar productos, marcas y más...'/>
+            <div>
+              sadcasd
+              <img></img>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    )
+  }
+}
+
+export default App;
