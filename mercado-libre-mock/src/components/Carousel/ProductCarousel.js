d6b8a58d0eb0fcc2f54f3f432841285da0c9661b
mabenunez <39781996+mabenunez@users.noreply.github.com>
2019-09-03 12:17:50 -0300

    css modules, router, MainIndex component

---
+++ mercado-libre-mock/src/components/Carousel/ProductCarousel.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import styles from './productCarousel.module.css';
+
+class ProductCarousel extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+
+        <div>
+
+        </div>
+
+    )
+  }
+}
+
+export default ProductCarousel;
